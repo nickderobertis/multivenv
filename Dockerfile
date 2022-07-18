77b2186089240f72f90816928abab1e77adc44c2
Nick DeRobertis <whoopnip@gmail.com>
2022-07-18 06:23:21 -0400

    feat: Add progress output for sync and compile

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,13 @@ COPY . .
 RUN pipenv run python upload.py --build-only
 RUN pip install dist/multivenv*.whl
 RUN mvenv --install-completion bash
-COPY tests/input_files/requirements.in cli-testing
+
+# Set up test cases
+RUN mkdir cli-testing/single
+COPY tests/input_files/requirements.in cli-testing/single
+COPY tests/input_files/mvenv.yaml cli-testing/single
+RUN mkdir cli-testing/multiple
+COPY tests/input_files/requirements.in cli-testing/multiple
+COPY tests/input_files/multiple/mvenv.yaml cli-testing/multiple
 
 WORKDIR cli-testing
--- multivenv/cli.py
+++ multivenv/cli.py
@@ -1,13 +1,15 @@
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Callable, Dict, Iterable, List, Optional, TypeVar
 
 import cliconf
 import typer
+from rich.progress import Progress
 
 from multivenv.compile import compile_venv_requirements
 from multivenv.config import VenvConfig, VenvUserConfig
 from multivenv.exc import MutlivenvConfigVenvsNotDefinedException, NoSuchVenvException
 from multivenv.run import run_in_venv
+from multivenv.styles import printer, styled
 from multivenv.sync import sync_venv
 
 cli = cliconf.CLIConf(name="mvenv")
@@ -42,8 +44,12 @@ def sync(
     venv_folder: Path = VENV_FOLDER_OPTION,
 ):
     venv_configs = _create_internal_venv_configs(venvs, venv_names, venv_folder)
-    for venv_config in venv_configs:
-        sync_venv(venv_config)
+    return _loop_sequential_progress(
+        venv_configs,
+        sync_venv,
+        lambda v: f"Syncing {v.name}",
+        lambda v: f"Synced {v.name}",
+    )
 
 
 @cli.command()
@@ -54,8 +60,12 @@ def compile(
     venv_folder: Path = VENV_FOLDER_OPTION,
 ):
     venv_configs = _create_internal_venv_configs(venvs, venv_names, venv_folder)
-    for venv_config in venv_configs:
-        compile_venv_requirements(venv_config)
+    return _loop_sequential_progress(
+        venv_configs,
+        compile_venv_requirements,
+        lambda v: f"Compiling {v.name}",
+        lambda v: f"Compiled {v.name}",
+    )
 
 
 def update():
@@ -116,6 +126,27 @@ def _create_internal_venv_configs(
         if name in venv_names
     ]
     return venv_configs
+
+
+T = TypeVar("T")
+
+
+def _loop_sequential_progress(
+    iterable: Iterable[T],
+    fn: Callable[[T], None],
+    before_message_fn: Callable[[T], str],
+    after_message_fn: Callable[[T], str],
+):
+    with Progress(console=printer.console, transient=True) as progress:
+        for item in iterable:
+            task = progress.add_task(
+                styled(before_message_fn(item), printer.styles["info"]),
+                start=False,
+                total=None,
+            )
+            fn(item)
+            progress.remove_task(task)
+            printer.success(after_message_fn(item))
 
 
 if __name__ == "__main__":
