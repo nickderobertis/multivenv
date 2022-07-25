a711b471324ad3ef3fe0b91f0df60f4104a06c06
Nick DeRobertis <whoopnip@gmail.com>
2022-07-24 20:03:38 -0400

    fix: Show stack trace when an unexpected error occurs using run_in_venv for internal commands

--- multivenv/_create.py
+++ multivenv/_create.py
@@ -1,13 +1,15 @@
 from multivenv._config import VenvConfig
 from multivenv._ext_subprocess import run
-from multivenv._run import run_in_venv
+from multivenv._run import ErrorHandling, run_in_venv
 from multivenv._state import create_venv_state
 
 
 def create_venv(config: VenvConfig):
     run(f"virtualenv {config.path}", stream=False)
     # Need pip-sync installed to install dependencies
-    run_in_venv(config, "pip install pip-tools", stream=False)
+    run_in_venv(
+        config, "pip install pip-tools", stream=False, errors=ErrorHandling.RAISE
+    )
     create_venv_state(config)
 
 
--- multivenv/_sync.py
+++ multivenv/_sync.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from multivenv._config import VenvConfig
 from multivenv._create import create_venv_if_not_exists
 from multivenv._find_reqs import find_requirements_file
-from multivenv._run import run_in_venv
+from multivenv._run import ErrorHandling, run_in_venv
 from multivenv._state import update_venv_state
 
 
@@ -15,5 +15,10 @@ def sync_venv(config: VenvConfig):
 def pip_tools_sync(config: VenvConfig) -> Path:
     create_venv_if_not_exists(config)
     requirements_file = find_requirements_file(config)
-    run_in_venv(config, f"pip-sync {requirements_file}", stream=False)
+    run_in_venv(
+        config,
+        f"pip-sync {requirements_file}",
+        stream=False,
+        errors=ErrorHandling.RAISE,
+    )
     return requirements_file
