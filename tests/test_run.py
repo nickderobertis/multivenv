aaa32ecd41fa7d55d5bf3f591f2ebb502e696d79
Nick DeRobertis <whoopnip@gmail.com>
2022-07-24 20:55:54 -0400

    chore: Add currently failing test that shows output is not contained in error from run

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,8 @@
-from multivenv._run import run_in_venv
+import subprocess
+
+from multivenv._run import ErrorHandling, run_in_venv
 from tests.fixtures.venvs import *
+from tests.osutils import is_not_found_output
 
 
 def test_run_in_venv(synced_venv: VenvConfig):
@@ -9,3 +12,12 @@ def test_run_in_venv(synced_venv: VenvConfig):
 def test_run_in_venv_with_emoji_output(synced_venv: VenvConfig):
     run_in_venv(synced_venv, "pip install black")
     assert "All done! ✨ 🍰 ✨" in run_in_venv(synced_venv, "black .")
+
+
+def test_run_with_error(synced_venv: VenvConfig):
+    with pytest.raises(subprocess.CalledProcessError) as exc_info:
+        run_in_venv(synced_venv, "sdfsdfsgsdfgsdfgf", errors=ErrorHandling.RAISE)
+
+    error = exc_info.value
+    assert error.returncode != 0
+    assert is_not_found_output(str(error))
