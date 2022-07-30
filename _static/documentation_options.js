bbea2512a12faf3e372ee70a7ffda1b3bf025b77
nickderobertis <nickderobertis@users.noreply.github.com>
2022-07-30 14:47:01 +0000

    Automated deployment: Sat Jul 30 14:47:01 UTC 2022 b46d6f1376ad831b870cbc6b4cb91dc9f89eb09b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0a24',
+    VERSION: '1.0.0a25',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
